c68f6d2030b7675be8e3705f9b94dd9837606a35
Rucha Bhopale <ruchab@coreflexsolutions.com>
2024-11-06 15:24:05 +0530

    Initial Commit

---
+++ Button/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Button
+//
+//  Created by apple on 30/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var setTextButtonTapped: UIButton!
+    @IBOutlet weak var clearTextButtonTapped: UIButton!
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func clearTextButtonTapped(_ sender: Any) {
+        label.text = ""
+    }
+    
+    @IBAction func setTextButtonTapped(_ sender: Any) {
+        label.text = textField.text
+    }
+}
+
---
+++ ButtonTests/ButtonTests.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonTests.swift
+//  ButtonTests
+//
+//  Created by apple on 30/10/24.
+//
+
+import Testing
+@testable import Button
+
+struct ButtonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
